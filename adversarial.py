3f0c0d0bcc9fd614e53ee33c87bcb401e34b77d7
Jiaxu Zou <zoujx96@gmail.com>
2018-09-27 01:41:38 -0400

    Update adversarial.py

--- adversarial.py
+++ adversarial.py
@@ -1,3 +1,12 @@
+'''
+!/usr/bin/env python
+-*- coding: utf-8 -*-
+**************************************
+@Time    : 2018/9/27 1:41
+@Author  : Jiaxu Zou
+@File    : adversarial.c 
+**************************************
+'''
 import pickle
 import tensorflow as tf 
 import numpy as np
